38f354a49e39dbb7de0828105fbf6bd71e2ce2e2
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-08-14 12:37:14 +0500

    Update AddToCartreducer.js

--- src/Store/Reducers/AddToCartreducer.js
+++ src/Store/Reducers/AddToCartreducer.js
@@ -1,20 +1,32 @@
-const InitialState = {
-    Cart: [],
-};
-
-export const AddToCartReducer = (state = InitialState, { type, payload }) => {
-    switch (type) {
-        case 'cart':
-            return {
-                ...state,
-                Cart: [...state.Cart, payload], 
-            };
-
-        case 'remove':
-            return {
-                ...state,
-                Cart: state.Cart.filter(item => item.id !== payload.id), 
-            };
+export const AddToCartReducer = (state = { Cart: [] }, action) => {
+    switch (action.type) {
+        case "cart": {
+            const existingIndex = state.Cart.findIndex(item => item.id === action.payload.id);
+            if (existingIndex >= 0) {
+                
+                const updatedCart = [...state.Cart];
+                updatedCart[existingIndex].quantity += 1;
+                return { ...state, Cart: updatedCart };
+            }
+       
+            return { ...state, Cart: [...state.Cart, { ...action.payload, quantity: 1 }] };
+        }
+        
+        case "remove": {
+            const existingIndex = state.Cart.findIndex(item => item.id === action.payload.id);
+            if (existingIndex >= 0) {
+                const updatedCart = [...state.Cart];
+                if (updatedCart[existingIndex].quantity > 1) {
+                    updatedCart[existingIndex].quantity -= 1;
+                    return { ...state, Cart: updatedCart };
+                } else {
+                 
+                    updatedCart.splice(existingIndex, 1);
+                    return { ...state, Cart: updatedCart };
+                }
+            }
+            return state;
+        }
 
         default:
             return state;
